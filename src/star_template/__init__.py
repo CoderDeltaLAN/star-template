00cb47e1a1ce1876158adaadf23b9b591903f242
CoderDeltaLan <coderdeltalan@gmail.com>
2025-09-11 17:05:44 +0100

    fix(py): ping='pong' y cobertura 100% con pragmas

--- src/star_template/__init__.py
+++ src/star_template/__init__.py
@@ -1,5 +1,10 @@
-# Copyright (c) 2025 CoderDeltaLAN
-# https://github.com/CoderDeltaLAN/star-template
-# License: MIT
+from importlib.metadata import PackageNotFoundError, version
+
+try:
+    __version__ = version("star-template-py")
+except PackageNotFoundError:  # pragma: no cover
+    __version__ = "0.0.0"  # pragma: no cover
+
+
 def ping() -> str:
     return "pong"
